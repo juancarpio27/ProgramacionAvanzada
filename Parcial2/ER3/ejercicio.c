a4d8ade762d655a5c54ca8f675f0e58464c1d179
juancarpio27 <juan.carpio01@gmail.com>
2015-10-15 15:26:15 -0500

    Examen

--- Parcial2/ER3/ejercicio.c
+++ Parcial2/ER3/ejercicio.c
@@ -43,6 +43,26 @@
 
 int grabar = 1;
 
+char* itoa(int i, char b[]){
+    char const digit[] = "0123456789";
+    char* p = b;
+    if(i<0){
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do{ //Move to where representation ends
+        ++p;
+        shifter = shifter/10;
+    }while(shifter);
+    *p = '\0';
+    do{ //Move back, inserting digits as u go
+        *--p = digit[i%10];
+        i = i/10;
+    }while(i);
+    return b;
+}
+
 void manejador_alarma(int id){
 	grabar = 0;
 }
@@ -53,6 +73,11 @@ int main(){
 	sigfillset(&todas);
 	sigdelset(&todas,SIGALRM);
 	sigprocmask(SIG_BLOCK,&todas,NULL);
+	struct sigaction nombre;
+	nombre.sa_handler = manejador_alarma;
+	nombre.sa_flags = SA_RESTART;
+	sigaction(SIGALRM,&nombre,0);
+
 
 	struct dirent *pDirent;
     DIR *pDir;
